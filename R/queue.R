9ec0dea8209ca1c6c7c70a8391ada77b742579b5
Scott Chamberlain <myrmecocystus@gmail.com>
2020-05-20 09:15:44 -0700

    fix #2 queueing rough draft in place

--- R/queue.R
+++ R/queue.R
@@ -1,36 +1,49 @@
 #' mutant queue
-#' @importFrom liteq xxx
-#' @importFrom rappdirs user_cache_dir
-#' @param x x
-#' @param x x
-queue <- function(temporary = TRUE) {
-  assert(temporary, "logical")
-  if (temporary) db_path <- tempfile()
-  if (!temporary) {
-    db_path <- file.path(rappdirs::user_data_dir("R/mutant"),
-      basename(tempfile()))
-  }
-  q <- liteq::ensure_queue("jobs", db = db_path)
-
-}
-
+#' @importFrom liteq ensure_queue publish try_consume
+#' @importFrom rappdirs user_data_dir
+#' @examples \dontrun{
+#' x <- queue$new()
+#' x
+#' x$q
+#' x$queue_path()
+#' x$messages()
+#' z <- list(
+#'   path = tempfile(), 
+#'   mutant_location = list(
+#'     `some-file.R` = 
+#'       list(line1 = 45, line2 = 46, column = 4, from = "==", to = ">")))
+#' x$publish(as.character(jsonlite::toJSON(z)))
+#' x$messages()
+#' mssg <- x$consume()
+#' mssg
+#' mssg$title
+#' mssg$message
+#' jsonlite::fromJSON(mssg$message)
+#' x$messages()
+#' x$done(mssg)
+#' x$messages()
+#' }
 queue <- R6::R6Class(
   'queue',
   public = list(
     q = NULL,
+    qpath = NULL,
+
     #' @description print method for `queue` objects
     #' @param x self
     #' @param ... ignored
     print = function(x, ...) {
       cat("<queue> ", sep = "\n")
-      for (i in seq_along(self$muts)) {
-        cat(paste0("  mutant name: ", self$muts[[i]]$name), sep = "\n")
-      }
+      cat(paste0("  queue path: ", self$qpath), sep = "\n")
       invisible(self)
     },
 
     #' @description Create a new queue object
     #' @return A new `queue` object
+    #' @param temporary (logical) create a temporary queue that is cleaned up
+    #' at the end of your R session? default: `TRUE`. if `FALSE`, we use
+    #' [rappdirs::user_data_dir()] to cache the file. use `$queue_path()`
+    #' to get the path for the queue
     initialize = function(temporary = TRUE) {
       assert(temporary, "logical")
       if (temporary) db_path <- tempfile()
@@ -38,18 +51,35 @@ queue <- R6::R6Class(
         db_path <- file.path(rappdirs::user_data_dir("R/mutant"),
           basename(tempfile()))
       }
+      self$qpath <- db_path
       self$q <- liteq::ensure_queue("jobs", db = db_path)
       return(self)
     },
 
-    publish = function(title, message) {
+    #' @description publish a job into the queue
+    #' @param message (character) job message, a JSON string with fields
+    #' `path` and `mutant_location`, for the file path to the mutated 
+    #' package to test and information on the location of the mutation,
+    #' respectively
+    #' @param title (character) job title, a random string
+    publish = function(message, title = uuid::UUIDgenerate()) {
+      assert(title, "character")
+      assert(message, "character")
       liteq::publish(self$q, title = title, message = message)
     },
 
-    consume = function() {
-      liteq::try_consume(self$q)
-    }
+    #' @description consume a job from the queue
+    consume = function() liteq::try_consume(self$q),
+
+    #' @description tell the queue the job can be removed from the queue
+    #' @param message (character) message object, of class `liteq_message`
+    done = function(message) liteq::ack(message),
+
+    #' @description list jobs in the queue
+    messages = function() liteq::list_messages(self$q),
+
+    #' @description fetch the queue path. `NULL` if there's no queue
+    queue_path = function() self$qpath
   ),
   private = list()
 )
-
