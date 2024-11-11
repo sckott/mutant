11d1547d1e2299b427bf5b2596cfad389a8b0f6f
Scott Chamberlain <sckott@fastmail.com>
2024-11-10 22:33:49 -0800

    pkg level doc

--- R/mutant-package.R
+++ R/mutant-package.R
@@ -1,9 +1,6 @@
-#' @title mutant
-#' @description Mutation Testing Framework
-#' @importFrom R6 R6Class
-#' @name mutant-package
-#' @aliases mutant
-#' @docType package
-#' @keywords package
-#' @author Scott Chamberlain
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+## usethis namespace: end
 NULL
